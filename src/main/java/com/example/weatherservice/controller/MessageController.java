b40b131602b08ada4a74b0bc32eb1d469fe2ce51
Haiwei Lu <newknowledgemail@gmail.com>
2021-07-29 00:01:37 +0800

    Optimized the format and content of the reply.

--- src/main/java/com/example/weatherservice/controller/MessageController.java
+++ src/main/java/com/example/weatherservice/controller/MessageController.java
@@ -20,6 +20,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.SignatureException;
 import java.util.Objects;
+import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA.
@@ -55,29 +56,57 @@ public class MessageController {
 
             StringBuilder message = new StringBuilder();
             if (Objects.equals(sender.getDemand(), "")) {
-                message.append("暂时没有找到哦,你试试发送 广州的天气 哦");
+                Random random = new Random();
+                int number = random.nextInt(4);
+                if (number == 0) {
+                    message.append("暂时没有找到哦,你试试发送 广州的天气 哦");
+                } else if (number == 1) {
+                    message.append("好深奥哦, 换个问题吧?");
+                } else if (number == 2) {
+                    message.append("啊? 这个我就不懂了哦");
+                } else {
+                    message.append("这也太难了");
+                }
+
             } else if (sender.getUserId() != null) {
                 WeatherResponse weatherResponse = weatherProvider.getDailyWeather(sender.getDemand());
                 message.append(sender.getDemand())
-                        .append(": \\r\\n")
-                        .append(" 白天天气状况: \\r\\n")
+                        .append("的天气: \r\n")
+                        .append("\r\n")
+                        .append("白天天气状况: ")
                         .append(weatherResponse.getDayWeather())
-                        .append(" 晚上天气状况: \\r\\n")
+                        .append("\r\n")
+                        .append("晚上天气状况: ")
                         .append(weatherResponse.getNightWeather())
-                        .append(" 最高温度: ")
+                        .append("\r\n")
+                        .append("最高温度: ")
                         .append(weatherResponse.getHighTemperature())
-                        .append(" 度\\r\\n")
-                        .append(" 最低温度: ")
+                        .append(" 度\r\n")
+                        .append("最低温度: ")
                         .append(weatherResponse.getLowTemperature())
-                        .append(" 度\\r\\n");
+                        .append(" 度\r\n")
+                        .append("湿度: ")
+                        .append(weatherResponse.getHumidity())
+                        .append("\r\n")
+                        .append("降雨量: ")
+                        .append(weatherResponse.getRainfall())
+                        .append("\r\n")
+                        .append("风力大小: ")
+                        .append(weatherResponse.getWindScale())
+                        .append(" 级\r\n")
+                        .append("风的角度: ")
+                        .append(weatherResponse.getWindDirectionDegree())
+                        .append(" 度\r\n");
+            } else {
+                return null;
             }
 
             OkHttpClient client = new OkHttpClient();
 
             String url = forwardUrl + "user_id=" + sender.getUserId()
-                    + "&message=" + message.toString();
+                    + "&message=" + URLEncoder.encode(message.toString(), StandardCharsets.UTF_8);
             Request request = new Request.Builder()
-                    .url(URLEncoder.encode(url, StandardCharsets.UTF_8))
+                    .url(url)
                     .build();
             try (Response response = client.newCall(request).execute()) {
                 System.out.println(Objects.requireNonNull(response.body()).string());
